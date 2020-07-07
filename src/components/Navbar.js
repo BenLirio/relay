2ed3b100349ea186c83155825e87829d18eb10a9
Ben Lirio <benjlirio@gmail.com>
2020-07-07 19:37:42 -0400

    Remove contact page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -69,24 +69,6 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/blog">
                 Tips & Advice
               </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
