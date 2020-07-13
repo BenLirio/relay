2a08a5e529ef264c670ee26fada2ed14b8e78f34
Ben Lirio <benjlirio@gmail.com>
2020-07-13 09:46:31 -0400

    Nav bar now uses material ui

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,80 +1,20 @@
 import React from 'react'
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
-const Navbar = class extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      active: false,
-      navBarActiveClass: '',
-    }
-  }
-
-  toggleHamburger = () => {
-    // toggle the active boolean in the state
-    this.setState(
-      {
-        active: !this.state.active,
-      },
-      // after state has been updated,
-      () => {
-        // set the class in state for the navbar accordingly
-        this.state.active
-          ? this.setState({
-            navBarActiveClass: 'is-active',
-          })
-          : this.setState({
-            navBarActiveClass: '',
-          })
-      }
-    )
-  }
-
-  render() {
-    return (
-      <nav
-        className="navbar is-transparent"
-        role="navigation"
-        aria-label="main-navigation"
-      >
-        <div className="container">
-          <div className="navbar-brand">
-            <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-            </Link>
-            {/* Hamburger menu */}
-            <div
-              className={`navbar-burger burger ${this.state.navBarActiveClass}`}
-              data-target="navMenu"
-              onClick={() => this.toggleHamburger()}
-            >
-              <span />
-              <span />
-              <span />
-            </div>
-          </div>
-          <div
-            id="navMenu"
-            className={`navbar-menu ${this.state.navBarActiveClass}`}
-          >
-            <div className="navbar-start has-text-centered">
-              <Link className="navbar-item" to="/about">
-                About
-              </Link>
-              <Link className="navbar-item" to="/how-to-help">
-                How To Help
-              </Link>
-              <Link className="navbar-item" to="/blog">
-                Tips & Advice
-              </Link>
-            </div>
-          </div>
-        </div>
-      </nav>
-    )
-  }
+const Navbar = () => {
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6">The Relay Resource</Typography>
+          <Button color="inherit" component={Link} to="/about">About</Button>
+          <Button color="inherit" component={Link} to="/how-to-help">How To Help</Button>
+          <Button color="inherit" component={Link} to="/blog">Tips & Advice</Button>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
 }
 
 export default Navbar
