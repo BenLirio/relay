047938add75085551d90b237b33642e0f64339da
Ben Lirio <benjlirio@gmail.com>
2020-07-14 12:13:40 -0400

    Clear extra config out

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,14 +27,6 @@ module.exports = {
       resolve: `gatsby-theme-material-ui`,
     },
     {
-      resolve: "gatsby-plugin-purgecss", // purges all unused/unreferenced css rules
-      options: {
-        develop: true, // Activates purging in npm run develop
-        purgeOnly: ["/all.sass"], // applies purging only on the bulma css file
-      },
-    }, // must be after other CSS plugins
-    "gatsby-plugin-netlify", // make sure to keep it last in the array
-    {
       resolve: "gatsby-source-wordpress",
       options: {
         baseUrl: "therelayresource.com",
