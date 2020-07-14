53e14d7454ce31f54c077c1310379d4d7383480b
Ben Lirio <benjlirio@gmail.com>
2020-07-14 12:45:49 -0400

    How to help sections

--- src/pages/how-to-help/index.js
+++ src/pages/how-to-help/index.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import { Typography, Container, Grid } from '@material-ui/core'
 
 const HowToHelp = () => {
   return (
-    <div>
-      <Typography variant="h1">How TO Help</Typography>
-    </div>
+    <>
+      <Container fixed>
+        <Grid container>
+          <Grid item xs={12}>
+            <Typography variant="h1">How to Help</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h2">How to Donate</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h2">How to Volunteer</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="h2">How to Advocate</Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
   )
 }
 
