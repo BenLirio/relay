e7707aeb313ffe9664c46811f1f7e7e9ec7aa350
Liam Richards <liaminmanila@gmail.com>
2020-07-16 01:38:22 +0800

    Added Home page how to help buttons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import FeatureImage from '../components/FeatureImage'
-import BlogRoll from '../components/BlogRoll'
-import CategoryBar from '../components/CategoryBar'
-import { Container, Grid, Typography } from '@material-ui/core'
+import React from "react"
+import FeatureImage from "../components/FeatureImage"
+import BlogRoll from "../components/BlogRoll"
+import CategoryBar from "../components/CategoryBar"
+import { Button, Container, Grid, Typography } from "@material-ui/core"
 
 const Relay = ({ data }) => {
   return (
@@ -12,12 +12,23 @@ const Relay = ({ data }) => {
       <Container fixed>
         <Grid container>
           <Grid item xs={12}>
-            <Typography variant="h2">{'Top Stories'}</Typography>
+            <Typography variant="h2">{"Top Stories"}</Typography>
           </Grid>
         </Grid>
         <BlogRoll width={4} height={1} excerpt={false} />
         <Typography variant="h2">Covid Map</Typography>
         <Typography variant="h2">How To Help</Typography>
+        <Grid container>
+          <Grid item xs={3} spacing={6}>
+            <Button>How to Help</Button>
+          </Grid>
+          <Grid item xs={3} spacing={6}>
+            <Button>How to Donate</Button>
+          </Grid>
+          <Grid item xs={3} spacing={6}>
+            <Button>How to Volunteer</Button>
+          </Grid>
+        </Grid>
       </Container>
     </div>
   )
