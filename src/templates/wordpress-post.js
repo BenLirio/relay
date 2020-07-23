c031a49babb5465b6fda2fc22ea82800d7f4222f
Liam Richards <liaminmanila@gmail.com>
2020-07-24 02:07:17 +0800

    Linked Posts to Home Page

--- src/templates/wordpress-post.js
+++ src/templates/wordpress-post.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import { Typography, Container } from '@material-ui/core'
+import React from "react"
+import { Link, graphql } from "gatsby"
+import { Typography, Container } from "@material-ui/core"
 
 const BlogPost = ({ data }) => {
   const { wpPost } = data
   return (
     <>
       <Container maxWidth="sm">
+        <Link to="/">Home Page</Link>
         <Typography variant="h2">{wpPost.title}</Typography>
         <div dangerouslySetInnerHTML={{ __html: wpPost.content }}></div>
       </Container>
